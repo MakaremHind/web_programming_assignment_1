d7068bca87e9b3347feadc0070800c4f68a5deca
Hind <hind.a.makarem@gmail.com>
2024-11-14 11:47:35 +0100

    Initial commit

--- run_server.py
+++ run_server.py
@@ -54,7 +54,7 @@ class S(SimpleHTTPRequestHandler):
                 {}
             """.format(self.path, post_data.decode('utf-8')).encode('utf-8'))
 
-def run(server_class=HTTPServer, handler_class=S, port=8000):
+def run(server_class=HTTPServer, handler_class=S, port=8080):
     logging.basicConfig(level=logging.INFO)
     server_address = ('', port)
     httpd = server_class(server_address, handler_class)
